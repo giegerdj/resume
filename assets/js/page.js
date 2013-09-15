eabe77b3a57e53c416b1d1b14a29aa7eeb5e658d
Dave Gieger <gieger.david@gmail.com>
2013-09-15 00:50:03 -0600

    saving my progress

---
+++ assets/js/page.js
@@ -0,0 +1,39 @@
+!function ($) {
+
+  $(function(){
+
+    var $window = $(window)
+    var $body   = $(document.body)
+
+    $body.scrollspy({
+      target: '.resume-sidebar',
+      offset: 10
+    })
+
+    $window.on('load', function () {
+      $body.scrollspy('refresh')
+    })
+
+    $('.resume-container [href=#]').click(function (e) {
+      e.preventDefault()
+    })
+    
+    // back to top
+    setTimeout(function () {
+      var $sideBar = $('.resume-sidebar')
+      
+      $sideBar.affix({
+        offset: {
+          top: function () {
+            var offsetTop      = $sideBar.offset().top
+            var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
+            
+            return (this.top = offsetTop - sideBarMargin)
+          }
+        , bottom: 0
+        }
+      })
+    }, 100)
+})
+
+}(window.jQuery)
