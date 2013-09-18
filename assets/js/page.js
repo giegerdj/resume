a566515d0b74a9168e62fbf8358afb5e9b97c1bf
Dave Gieger <gieger.david@gmail.com>
2013-09-18 05:51:07 +0000

    saving a lot of new content

--- assets/js/page.js
+++ assets/js/page.js
@@ -2,38 +2,51 @@
 
   $(function(){
 
-    var $window = $(window)
-    var $body   = $(document.body)
+    var $window = $(window);
+    var $body   = $(document.body);
 
     $body.scrollspy({
       target: '.resume-sidebar',
       offset: 10
-    })
+    });
 
     $window.on('load', function () {
       $body.scrollspy('refresh')
-    })
-
-    $('.resume-container [href=#]').click(function (e) {
-      e.preventDefault()
-    })
+    });
     
     // back to top
     setTimeout(function () {
-      var $sideBar = $('.resume-sidebar')
+      var $sideBar = $('.resume-sidebar');
       
       $sideBar.affix({
         offset: {
           top: function () {
-            var offsetTop      = $sideBar.offset().top
-            var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
+            var offsetTop      = $sideBar.offset().top;
+            var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10);
             
-            return (this.top = offsetTop - sideBarMargin)
+            return (this.top = offsetTop - sideBarMargin);
           }
         , bottom: 0
         }
       })
-    }, 100)
-})
+    }, 100);
+    
+    $.each($('.gauge'), function(index, el) {
+      var title = $(el).attr('data-title');
+      var id = $(el).attr('id');
+      var value = $(el).attr('data-value');
+      
+      var g = new JustGage({
+        id: id, 
+        value: value, 
+        min: 0,
+        max: 100,
+        title: title,
+        titleFontColor: '#333333',
+        levelColors: ['#cccccc','#004400']
+      });
+    });
+    
+});
 
 }(window.jQuery)
